6d9b585f0102e05e025c1b94f33b862620894829
dsharp <dsshar@gmail.com>
2018-02-11 21:05:15 -0500

    new prefabs

---
+++ Assets/Scripts/NewWallTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewWallTest : MonoBehaviour {
+	public MazeWall wallPrefab;
+	// Use this for initialization
+	void Start () {
+		MazeWall wall = Instantiate (wallPrefab) as MazeWall;
+		wall.transform.parent = this.transform;
+		wall.transform.localPosition = Vector3.zero;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
